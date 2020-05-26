4f3aaf712d04596f3fbad6eff42c8acbd44eb472
chixinning <xinning_chi@163.com>
2020-05-26 20:31:29 +0800

    GRPC

---
+++ pythonGRPC/client.py
@@ -0,0 +1,13 @@
+import grpc
+import Network_pb2
+import Network_pb2_grpc
+ 
+IP = 'localhost:9091'
+def run():
+    channel = grpc.insecure_channel(IP)
+    stub =  Network_pb2_grpc.NetworkStub(channel=channel)
+    response = stub.CallBack(Network_pb2.Request(clientName='wwq'))
+    print("recevied: " + response.message)
+ 
+if __name__ == '__main__':
+    run()
